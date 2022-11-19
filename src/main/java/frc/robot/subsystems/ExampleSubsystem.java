9de592e7009528311f26c51eb5ac2ec6fcb7f99c
GarrettGerver <91382301+superxman1@users.noreply.github.com>
2022-11-19 11:26:59 -0500

    added camera tracking no pid

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -7,19 +7,44 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
   
   TalonSRX _motor;
+  PhotonCamera _cam;
   public ExampleSubsystem() {
     _motor = new TalonSRX(5);
+    _cam = new PhotonCamera("AprilTagCam");
+   
     
   }
 
   @Override
   public void periodic() {
-    _motor.set(ControlMode.PercentOutput, .2);
+    var result = _cam.getLatestResult();
+    if (result.hasTargets())
+    {
+      PhotonTrackedTarget _target = result.getBestTarget();
+      double _yaw = _target.getYaw();
+      if(_yaw < -6){
+        _motor.set(ControlMode.PercentOutput, .1);
+        System.out.println(_yaw);
+      } else if(_yaw > 6){
+        _motor.set(ControlMode.PercentOutput, -.1);
+        System.out.println(_yaw);
+      } else {
+        _motor.set(ControlMode.PercentOutput, 0);
+      }
+    } else {
+      _motor.set(ControlMode.PercentOutput, 0);
+    }
+    
+    
+    //_motor.set(ControlMode.PercentOutput, .2);
     // This method will be called once per scheduler run
   }
 
